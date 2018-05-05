e5e9b3759b37d2a05c3fd4f6692e5e6452434885
Mike <mikec201722@gmail.com>
2018-05-05 16:45:11 -0700

    Commented code
    Commented code, fixing part 3(). Need to rename

--- MainCodeArea.java
+++ MainCodeArea.java
@@ -6,120 +6,223 @@ public class MainCodeArea {
 
 	public MainCodeArea(){}
 
+	/*
+	reads input from input3.txt file. 
+	reads input by line and puts each line into a string ArrayList. 
+	then passes the String ArrayList to 
+	*/
 	public void readFile(){
 		String line = null;
-		ArrayList<String> array = new ArrayList<String>(); 
+		ArrayList<String> input = new ArrayList<String>(); 
 		try{
-			FileReader fileReader = new FileReader("input1.txt");
+			FileReader fileReader = new FileReader("input3.txt");
 			BufferedReader bufferedReader = new BufferedReader(fileReader);  
 			while( ( line = bufferedReader.readLine() ) != null ){
-				array.add(line);
+				input.add(line);
 			}
 		}
 		catch(FileNotFoundException exFileNotFound){
-			System.out.println("input1.txt cant find");
+			System.out.println("input3.txt cant find");
 		}
 		catch(IOException exIO){
-			System.out.println("cantReadFile");
+			System.out.println("cant Read File!");
 		}
-		part1(array);
+		initalizeXValues(input);
 	}
+	/*
+	creates an int array to hold all the x values
+	*/
 
-	public void part1(ArrayList<String> ar){
-		ArrayList<Integer> xVarList = new ArrayList<Integer>();
+	public void initalizeXValues(ArrayList<String> input){
+		int[] clausesLeft = new int[input.size()];
+		int[] clausesRight = new int[input.size()];
+		ArrayList<Integer> clauses = new ArrayList<Integer>();
 		String[] line;
-		int temp;
 
-		for(int i = 0; i < ar.size(); i++){
-			line=ar.get(i).split(" ");
-			temp = Integer.parseInt(line[0]);
-			xVarList.add(temp);
-			temp = Integer.parseInt(line[1]);
-			xVarList.add(temp);     
+		for(int i = 0; i < input.size(); i++){
+			line = input.get(i).split(" ");
+			//clausesLeft[i] = Integer.parseInt(line[0]);
+			//clausesRight[i] = Integer.parseInt(line[1]);
+			clauses.add(Integer.parseInt(line[0]));
+			clauses.add(Integer.parseInt(line[1]));     
 		}
-		for(int i = 0; i < xVarList.size(); i++) {   
-			System.out.print(xVarList.get(i) + " ");
+		for(int i = 0; i < clauses.size(); i++) {   
+			System.out.print(clauses.get(i) + " ");
 			i++;
-			System.out.print(xVarList.get(i) + "\n");
+			System.out.print(clauses.get(i) + "\n");
+			// System.out.print(clausesLeft[i] + " ");
+			// System.out.print(clausesRight[i] + "\n");
 		} 
-		sortValues(xVarList);
+		getTableNArrays(clauses);
 	}
 
-	public boolean checkIfItExists(ArrayList<Integer> absValueList,int temp){
+	/*
+	returns true if a member of the ArrayList Integer exists 
+	retruns false if it doesnt 
+	*/
+	public boolean checkIfItExists(ArrayList<Integer> ar,int temp){
 
-		for(int i = 0; i < absValueList.size(); i++){
-			if(temp ==absValueList.get(i))
+		for(int i = 0; i < ar.size(); i++)
+			if(temp ==ar.get(i))
 				return true;
+
+			return false;
 		}
-		return false;
+	/*
+		creates an array of all x variables used 
+		and a truth table 
+		with the corresponding not values
+	*/
+		public void getTableNArrays(ArrayList<Integer> clauses){
+			ArrayList<Integer> absValueList = new ArrayList<Integer>();
+			int[][] truthTable;
 
+			int temp,totalNums = 0;
 
-	}
+			for(int i = 0; i < clauses.size(); i++){
+				temp = Math.abs(clauses.get(i));
+				if(checkIfItExists(absValueList,temp) == false){
+					absValueList.add(temp);
+					totalNums++;
+					absValueList.add((temp * -1));
+				}
+		} //Collections.sort(absValueList); // sort
 
-	public void sortValues(ArrayList<Integer> xVarList){
-		ArrayList<Integer> absValueList = new ArrayList<Integer>();
-
-		int temp;
-		int totalNums = 0;
-
-
-		for(int i = 0; i < xVarList.size(); i++){
-			temp = Math.abs(xVarList.get(i));
-			if(checkIfItExists(absValueList,temp) == false){
-				absValueList.add(temp);
-				totalNums++;
-				absValueList.add((temp * -1));
-			}
-		}
-
-		//Collections.sort(absValueList); // sort
-
-		for(int i = 0; i < absValueList.size(); i++) {   
-			System.out.print(absValueList.get(i) + " ");
-		} 
-
-		System.out.println("\n*****************\n");
-		next(absValueList,totalNums);
-
-
+		truthTable = createTruthTable(absValueList,totalNums);
+		ArrayList<Integer> xVarUsed = xVarUsed(clauses, absValueList);
 		
+		print2dArray(totalNums, truthTable);
+
+		//part3(clauses,truthTable,xVarUsed);
 	}
 
-	public void next(ArrayList<Integer> absValueList,int totalNums){
-		String[][] truthTable = new String[(int)((Math.pow(2,totalNums))+1)][totalNums*2];
-		int ans =0, counter = 0;
-
-		for(int col = 0; col < (totalNums*2) ; col++){
-			truthTable[0][col] = "" + absValueList.get(col);
-		}
-
-		for (int row=1; row<(int)((Math.pow(2,totalNums))+1); row++) {
-			counter = 0;
-			for (int col=n-1; col>=0; col--) {
-				ans = ((row-1)/ ((int) Math.pow(2, col)))%2 ;
-				truthTable[row][counter] = "" + ans;
-				counter++;
-				if(ans == 0)
-					ans = 1;
-				else 
-					ans = 0;
-				truthTable[row][counter] = "" + ans;
-				counter++;
-			}
-		}
-
-		for (int row = 0; row < (int)((Math.pow(2,totalNums))+1)  ; row++) {
-			for (int col = 0 ;col < (totalNums*2) ;  col++) {
+	/*
+	prints a 2d array 
+	*/
+	public void print2dArray(int totalNums, int[][] truthTable){
+		for (int row = 0; row < truthTable.length ; row++) {
+			//System.out.print(row+" :");
+			for (int col = 0 ;col < truthTable[0].length ;  col++) {
 				System.out.print(truthTable[row][col] + "\t");
-
 			}
 			if(row == 0)
-				System.out.print("\n--------------");
+				System.out.print("\n-------------------------");
 			System.out.println();
 		}
 
 	}
 
+	/*
+	creates a truth table with the coresspoding not values
+	*/
+	public int[][] createTruthTable(ArrayList<Integer> absValueList,int totalNums){
+		int[][] truthTable = new int[(int)((Math.pow(2,totalNums))+1)][totalNums*2];
+		int ans =0, counter = 0;
 
+		for(int col = 0; col < (totalNums*2) ; col++){
+			truthTable[0][col] = absValueList.get(col);
+		}
 
-}            
+		for (int row=1; row<(int)((Math.pow(2,totalNums))+1); row++) {
+			counter = 0;
+			for (int col=totalNums-1; col>=0; col--) {
+				ans = ((row-1)/ ((int) Math.pow(2, col)))%2 ;
+				if(ans == 0) ans = 1;
+				else ans = 0;
+				truthTable[row][counter] = ans;
+				counter++;
+				if(ans != 0) ans = 0;
+				else ans = 1;	
+				truthTable[row][counter] = ans;
+				counter++;
+			}
+		}
+		return truthTable;
+	}
+
+	//public boolean findIndex()
+	/*
+	creates the an array list with only the x variables used within the input
+	*/
+	public ArrayList<Integer> xVarUsed(ArrayList<Integer> clauses, ArrayList<Integer> absValueList){
+		int temp = 0;
+		boolean checkIfItExists = true;
+		ArrayList<Integer> xVarUsed = new ArrayList<Integer>();
+
+		for(int i = 0; i < absValueList.size(); i++){
+			temp = absValueList.get(i);
+			checkIfItExists = checkIfItExists(clauses,temp);
+			if(checkIfItExists)
+				xVarUsed.add(temp);
+		}
+		return xVarUsed; 
+	}
+
+	public void part3(ArrayList<Integer> clauses, int[][] truthTable, ArrayList<Integer> xVarUsed ){
+		//0 = false
+		//1 = true
+
+		int bool1=0,bool2=0,counter = 0;
+		boolean xBool1 = true, xBool2 = true, result = true;
+		System.out.println(xVarUsed);
+		System.out.println(xVarUsed.size() * 2);
+
+		int x1 = 0, x1Not = 0;
+		int x2 = 0, x2Not = 0;
+		boolean xOneBool = true, xOneBoolNot = true;
+		boolean xTwo = true, xTwoNot = true;
+		int c1 = 0;
+		int c2 = 0;
+
+		for(int xBeingUsed= 0; xBeingUsed < xVarUsed.size(); xBeingUsed++){ // loops through all the xVar used 
+			x1 = xVarUsed.get(xBeingUsed);
+			if (choice == 0){
+				if(x1 > 0) xOneBool = true; // if x1 positive then true
+				else xOneBool = false; //else if negative then false
+			}
+			else{
+				if(x1 < 0) xOneBool = true; // if x1 negative then true
+				else xOneBool = false; // else if positive then false
+			}
+			for (int i = 0; i < clauses.size()   ; i++){ // gets clauses
+				c1 = clauses.get(i);
+				i++;
+				c2 = clauses.get(i);
+				if (c1 == x1){
+					// use xOneBool
+
+				}
+				if (c2 == x1){
+
+				}
+					//if c1 == xVar used 
+					// if c2 == xvar used 
+					// truth table
+			}
+			for (int column = 0;column < (clauses.size()/2)  ; column++) {
+
+			}	
+
+		}
+	}
+
+		// for(int i = 0; i < clauses.size(); i++){
+		// 	bool1 = clauses.get(i);
+		// 	i++;
+		// 	bool2 = clauses.get(i);
+
+		// 	if(bool1>0)
+		// 		xBool1 =true;
+		// 	else
+		// 		xBool1 = false;
+		// 	if (bool2 > 0)
+		// 		xBool2 = true;
+		// 	else
+		// 		xBool2=false;
+
+		// 	result = (xBool2 || xBool1);
+		// 	if(result)
+		// 		counter++;
+		// }
+		//System.out.println("\n"+counter);
+}
