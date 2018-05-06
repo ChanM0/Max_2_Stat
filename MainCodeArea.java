7e3587598a1246cc85bf28552f10085de8276938
Mike <mikec201722@gmail.com>
2018-05-05 19:53:05 -0700

    Found Opt Solution
    Finished part3 method

--- MainCodeArea.java
+++ MainCodeArea.java
@@ -93,7 +93,7 @@ public class MainCodeArea {
 		
 		print2dArray(totalNums, truthTable);
 
-		//part3(clauses,truthTable,xVarUsed);
+		part3(clauses,truthTable,xVarUsed);
 	}
 
 	/*
@@ -123,7 +123,7 @@ public class MainCodeArea {
 			truthTable[0][col] = absValueList.get(col);
 		}
 
-		for (int row=1; row<(int)((Math.pow(2,totalNums))+1); row++) {
+		for (int row = 1; row<(int)((Math.pow(2,totalNums))+1); row++) {
 			counter = 0;
 			for (int col=totalNums-1; col>=0; col--) {
 				ans = ((row-1)/ ((int) Math.pow(2, col)))%2 ;
@@ -158,53 +158,100 @@ public class MainCodeArea {
 		return xVarUsed; 
 	}
 
+	public int getCol (int[][] truthTable, int row, int c1){
+		//boolean value;
+		for(int columnValue = 0; columnValue < truthTable[0].length ; columnValue++){
+			if( c1 == truthTable[0][columnValue])
+				return columnValue;
+		}
+
+		return -10;
+
+		//return true; 
+
+	}
 	public void part3(ArrayList<Integer> clauses, int[][] truthTable, ArrayList<Integer> xVarUsed ){
 		//0 = false
 		//1 = true
 
-		int bool1=0,bool2=0,counter = 0;
-		boolean xBool1 = true, xBool2 = true, result = true;
 		System.out.println(xVarUsed);
 		System.out.println(xVarUsed.size() * 2);
 
-		int x1 = 0, x1Not = 0;
-		int x2 = 0, x2Not = 0;
-		boolean xOneBool = true, xOneBoolNot = true;
-		boolean xTwo = true, xTwoNot = true;
-		int c1 = 0;
-		int c2 = 0;
+		int c1Bool = 1, c2Bool = 1;
+		int c1 = 0, c2 = 0;
+		int columnValue = 0, truthCounter =0, max = 0, temp = 0;
 
-		for(int xBeingUsed= 0; xBeingUsed < xVarUsed.size(); xBeingUsed++){ // loops through all the xVar used 
-			x1 = xVarUsed.get(xBeingUsed);
-			if (choice == 0){
-				if(x1 > 0) xOneBool = true; // if x1 positive then true
-				else xOneBool = false; //else if negative then false
+		boolean test = false;
+		System.out.println();
+
+
+
+		for(int row = 1; row < truthTable.length; row++){
+			truthCounter = 0;
+			System.out.println("ROW :" +row);
+			
+			for (int clos = 0; clos < clauses.size() ;clos++ ) {
+				c1 = clauses.get(clos);
+				clos++;
+				c2 = clauses.get(clos);
+
+				columnValue = getCol(truthTable, row, c1);
+				temp = truthTable[row][columnValue ];
+				c1Bool = temp;
+
+				columnValue = getCol(truthTable, row, c2);
+				temp = truthTable[row][columnValue ];
+				c2Bool = temp;
+
+				test = (c1Bool == 1) || (c2Bool == 1);
+				System.out.println("c1 : "+c1+ " = "+c1Bool+"\tc2 : "+c2+ " = "+c2Bool+"\tboolean comp: "+test );
+
+				if(test)
+					truthCounter++;
+
 			}
-			else{
-				if(x1 < 0) xOneBool = true; // if x1 negative then true
-				else xOneBool = false; // else if positive then false
-			}
-			for (int i = 0; i < clauses.size()   ; i++){ // gets clauses
-				c1 = clauses.get(i);
-				i++;
-				c2 = clauses.get(i);
-				if (c1 == x1){
-					// use xOneBool
-
-				}
-				if (c2 == x1){
-
-				}
-					//if c1 == xVar used 
-					// if c2 == xvar used 
-					// truth table
-			}
-			for (int column = 0;column < (clauses.size()/2)  ; column++) {
-
-			}	
-
+			max = Math.max(truthCounter,max);
+			System.out.println();
 		}
+
+
+
+		System.out.println("Max truths "+max);
 	}
+
+	// for(int xBeingUsed= 0; xBeingUsed < xVarUsed.size(); xBeingUsed++){	 // loops through all the xVar used 
+		// 	x1 = xVarUsed.get(xBeingUsed);
+		// 	// for (int choice = 0; choice < 2 ; choice++ ) {
+
+
+		// 	// 	if (choice == 0){
+		// 	// 		if(x1 > 0) xOneBool = true; // if x1 positive then true
+		// 	// 		else xOneBool = false; //else if negative then false
+		// 	// 	}
+		// 	// 	else{
+		// 	// 		if(x1 < 0) xOneBool = true; // if x1 negative then true
+		// 	// 		else xOneBool = false; // else if positive then false
+		// 	// 	}
+		// 		for (int i = 0; i < clauses.size() ; i++){ // gets clauses
+		// 			c1 = clauses.get(i);
+		// 			i++;
+		// 			c2 = clauses.get(i);
+		// 			if (c1 == x1){
+		// 				// use xOneBool
+
+		// 			}
+		// 			if (c2 == x1){
+
+		// 			}
+		// 				//if c1 == xVar used 
+		// 				// if c2 == xvar used 
+		// 				// truth table
+		// 		}
+		// 		for (int column = 0;column < (clauses.size()/2)  ; column++) {
+
+		// 		}
+		// 	//}		
+		// }
 
 		// for(int i = 0; i < clauses.size(); i++){
 		// 	bool1 = clauses.get(i);
